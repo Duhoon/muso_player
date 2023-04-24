3400f4856dc3b14dc45794d9b448055eabab8662
412ock <412ock.dev@gmail.com>
2023-04-25 03:13:47 +0900

    feat: Waveform Draw

--- index.js
+++ index.js
@@ -6,6 +6,45 @@ const audioElement = document.querySelector("audio");
 const track = audioContext.createMediaElementSource(audioElement);
 track.connect(audioContext.destination);
 
+(function(){
+   fetch("music.mp3")
+    .then( response => response.arrayBuffer() )
+    .then( arraybuffer => {
+        audioContext.decodeAudioData(arraybuffer, buffer => {
+            currentBuffer = buffer;
+            displayBuffer(buffer);
+        })
+    } )
+})()
+
+// CANVAS
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+const canvasWidth = 500, canvasHeight = 300;
+
+
+async function displayBuffer(buff){
+    let leftChannel = buff.getChannelData(0);
+    let lineOpacity = canvasWidth / leftChannel.length ;
+    context.save();
+    context.fillStyle = "#222";
+    context.fillRect(0, 0, canvasWidth, canvasHeight);
+    context.strokeStyle = "#121";
+    context.globalCompositeOperation = 'lighter';
+    context.translate(0, canvasHeight / 2);
+    context.globalAlpha = 0.06 ;
+    for ( let i = 0 ; i < leftChannel.length; i++){
+        var x = Math.floor( canvasWidth * i / leftChannel.length );
+        var y = leftChannel[i] * canvasHeight / 2 ;
+        context.beginPath();
+        context.moveTo( x, 0);
+        context.lineTo(x+1, y);
+        context.stroke();
+    }
+    context.restore();
+    console.log("done");
+}
+
 const playBtn = document.querySelector("button");
 playBtn.addEventListener('click', function(){
     if(audioContext.state === 'suspended'){
@@ -27,6 +66,8 @@ playBtn.addEventListener('click', function(){
         target.add("fa-play")
     }
 }, false);
+
+
 
 const pauseBtn = document.createElement("div");
 const stopBtn = document.createElement("div");
